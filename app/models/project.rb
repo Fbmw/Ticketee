cf634422994342cf720ff875431d2d510e5c5588
Franklin Monroy <franklin@Franklins-MacBook-Pro.local>
2016-05-14 07:26:46 -0700

    create a new project's feature complete

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+
+#this create a table on database
+	def change
+		create_table :projects do |t|
+			t.string :name
+			t.string :description
+			t.timestamps null: false
+	    end
+	end
 end
